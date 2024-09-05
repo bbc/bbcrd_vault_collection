ada1dc67d51449e13a744ca23548901bbc176426
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2024-09-05 09:21:14 +0100

    Add vault_oidc_configure and vault_oidc_roles modules.

---
+++ plugins/modules/vault_oidc_configure.py
@@ -0,0 +1,158 @@
+import os
+import json
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.bbcrd.ansible_vault.plugins.module_utils.vault import (
+    get_vault_api_request_argument_spec,
+    vault_api_request,
+)
+from ansible_collections.bbcrd.ansible_vault.plugins.module_utils.dict_compare import (
+    dict_issubset,
+)
+
+
+DOCUMENTATION = r"""
+module: bbcrd.ansible_vault.vault_oidc_configure
+
+short_description: Configure the OIDC auth method.
+
+options:
+    config:
+        description: |-
+            The top-level OIDC parameters, see the Vault docs:
+            https://developer.hashicorp.com/vault/api-docs/auth/jwt#parameters
+        required:
+        type: dict
+    status:
+        description: |-
+            If 'present' (the default), will only write the configuration if no
+            configuration exists. Otherwise, no action will be performed. This
+            is the default behaviour because the Vault API does not allow
+            reading of the complete OIDC config (e.g. the secrets) so there is
+            no way to detect changes.
+            
+            If 'updated', will overwrite any existing configuration.
+    mount:
+        description: |-
+            The mountpoint of the OIDC auth method (without a trailing
+            slash).
+        required: false
+        type: str
+        default: "oidc"
+    vault_url:
+        description: The base URL of the vault server. Defaults to the contents
+        of the VAULT_ADDR environment variable.
+        required: false
+        type: str
+    vault_token:
+        description: Token to use for Vault API calls. Defaults to the contents
+        of the VAULT_TOKEN environment variable.
+        required: false
+        type: str
+    vault_ca_path:
+        description: |-
+            The filename of the CA PEM file to use. Leave blank to use the
+            built in certificate store. Defaults to the the VAULT_CACERT
+            environment variable.
+        required: false
+        type: str
+        default: null
+"""
+
+EXAMPLES = r"""
+- name: Enable OIDC auth endpoint
+  bbcrd.ansible_vault.vault_auth_methods:
+    auth_methods:
+      oidc:
+        type: oidc
+
+- name: Configure OIDC
+  bbcrd.ansible_vault.vault_oidc_configure:
+    config:
+      oidc_discovery_url: "https://sso-nbe.bbcis.uk/realms/example"
+      oidc_discovery_ca_pem: |-
+        -----BEGIN CERTIFICATE-----
+        MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+        TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+        cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+        WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+        ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+        MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+        h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+        0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+        A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+        T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+        B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+        B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+        KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+        OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+        jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+        qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+        rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+        HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+        hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+        ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+        3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+        NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+        ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+        TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+        jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+        oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+        4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+        mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+        emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+        -----END CERTIFICATE-----
+      oidc_client_id: "vault"
+      oidc_client_secret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+      default_role: default
+"""
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        config=dict(type="dict", default={}),
+        state=dict(
+            type="str",
+            choices=[
+                "present",
+                "updated",
+            ],
+            default="present",
+        ),
+        mount=dict(type="str", default="oidc"),
+        **get_vault_api_request_argument_spec(),
+    )
+
+    module = AnsibleModule(argument_spec=module_args)
+    result = {"changed": False}
+
+    config = module.params["config"]
+    state = module.params["state"]
+    mount = module.params["mount"]
+
+    response = vault_api_request(
+        module,
+        f"/v1/auth/{mount}/config",
+        method="GET",
+        expected_status=[200, 404],
+    )
+    already_configured = "data" in response
+
+    if not already_configured or state == "updated":
+        result["changed"] = True
+        response = vault_api_request(
+            module,
+            f"/v1/auth/{mount}/config",
+            method="POST",
+            data=config,
+        )
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
