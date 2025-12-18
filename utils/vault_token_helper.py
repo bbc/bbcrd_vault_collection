6a4190da6ef59ea07ad2e44936f053c10f6dd0b0
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2025-12-18 14:07:05 +0000

    vault_token_helper: Atomically update tokens file
    This change fixes issues where multiple token helpers running in parallel may
    see an empty tokens file whilst it is being re-written.
    
    As an extra improvement, we also skip writing the tokens file on 'get',
    dramatically reducing the number of file writing operations required.

--- utils/vault_token_helper.py
+++ utils/vault_token_helper.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import json
 import os
 import sys
+import tempfile
 
 tokens_file = Path.home() / ".vault-tokens"
 
@@ -25,18 +26,26 @@ def main() -> None:
             server_address = os.environ[var]
 
     if tokens_file.is_file():
-        tokens = json.load(tokens_file.open())
+        tokens = json.load(tokens_file.open(encoding="utf-8"))
     else:
         tokens = {}
 
     if args.action == "get":
         print(tokens.get(server_address, ""), end="")
+        return  # Exit early, no changes to write-back
     elif args.action == "store":
         tokens[server_address] = sys.stdin.read()
     elif args.action == "erase":
         tokens.pop(server_address, None)
 
-    json.dump(tokens, tokens_file.open("w"))
+    # Atomically overwrite the tokens file
+    temp_fnum, temp_fname = tempfile.mkstemp(
+        prefix=tokens_file.name,
+        dir=tokens_file.parent,
+    )
+    os.write(temp_fnum, json.dumps(tokens).encode("utf-8"))
+    os.close(temp_fnum)
+    os.replace(temp_fname, tokens_file)
 
 
 if __name__ == "__main__":
